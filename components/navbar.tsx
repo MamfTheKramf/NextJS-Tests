777243d3f893f5fdc2f100e3c752f8c8fc5befa5
Mamf <contact@mschrauth.de>
2023-07-01 11:39:25 +0200

    add navbar controls

---
+++ components/navbar.tsx
@@ -0,0 +1,83 @@
+"use-client"
+
+import Link from "next/link";
+import Script from "next/script";
+import styles from "./navbar.module.scss";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
+
+type LinkEntry = {
+    text: string;
+    path: string;
+}
+
+const links: LinkEntry[] = [
+    {
+        text: 'Home',
+        path: '/',
+    },
+    {
+        text: 'About Us',
+        path: '/about-us',
+    },
+    {
+        text: 'Extra',
+        path: '/extra',
+    },
+]
+
+export default function Navbar() {
+    const path = useActive();
+    return (
+        <>
+            <nav className={styles.nav}>
+                <ul className={styles.wide}>
+                    {links.map(link => (
+                        <li>
+                            <Link href={link.path} className={clsx({
+                                [styles.active]: isActive(link.path, path)
+                            })}>
+                                {link.text}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <ul className={styles.narrow}>
+                    {links.map(link => (
+                        <li>
+                            <Link href={link.path} className={clsx({
+                                [styles.active]: isActive(link.path, path)
+                            })}>
+                                {link.text}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+function useActive(): string {
+    const [active, setActive] = useState("");
+    useEffect(() => {
+        
+        function handleLoad() {
+            console.log(window.location.pathname);
+            setActive(window.location.pathname);
+        }
+
+        handleLoad();
+
+        return () => window.removeEventListener("resize", handleLoad);
+    });
+
+    return active;
+}
+
+function isActive(neededPrefix: string, actualPath: string): boolean {
+    if (!neededPrefix || neededPrefix === "/") {
+        return !actualPath || actualPath === "/";
+    }
+    return actualPath.startsWith(neededPrefix);
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import Navbar from '@/components/navbar'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log(pageProps);
+  return (
+    <>
+    <Navbar />
+    <Component {...pageProps} />
+    </>
+  )
 }
