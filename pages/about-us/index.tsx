485b062010756cbe69f12fccd3721a781fca356b
Mamf <contact@mschrauth.de>
2023-07-01 18:23:27 +0200

    add php

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import styles from '../../styles/AboutUs.module.scss';
 
+
 export default function AboutUs() {
+
     return (
         <>
             <div className={styles.about_us}>
                 <h1>
                     About Us
                 </h1>
+                <form action="/php/test.php" method="post">
+                    <input type="text" name="in_test" id="txt" />
+                    <input type="submit" value="Send" />
+                </form>
             </div>
         </>
     );
